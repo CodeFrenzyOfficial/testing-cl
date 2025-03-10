26a246c8b29ecaf8c8a6bd396f30174a76d08afb
CodeFrenzyOfficial <teamcodefrenzy@gmail.com>
2025-03-11 03:38:13 +0500

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -58,7 +58,7 @@ export default function Home() {
 
           {/* cat video */}
           <div className="w-full h-full relative grid place-items-center custom-gradient z-[100]">
-            <div className="w-2/3 lg:w-[40%] rounded-full overflow-hidden">
+            <div className="w-2/3 lg:w-full aspect-video rounded-full overflow-hidden">
               <video autoPlay loop muted playsInline>
                 <source src={`/assets/animation/cat-animation.mp4?v=${Date.now()}`} type="video/mp4" />
               </video>
