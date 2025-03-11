4e71b649254a0d58e03fbcbfee211ebcb79d6694
CodeFrenzyOfficial <teamcodefrenzy@gmail.com>
2025-03-12 03:44:08 +0500

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -11,8 +11,7 @@ export default function Home() {
       <Loader />
       <Nav />
       <main className="relative min-h-screen">
-        {/* pattern */}
-        <img src="/assets/icon-images/bg-pattern.png" className="absolute top-0 left-0 w-full h-full object-cover z-20 pointer-events-none" alt="" />
+
 
         {/* background video */}
         <video
@@ -21,12 +20,14 @@ export default function Home() {
           loop
           autoPlay
           muted
+          controls={false}
+          playsInline
         />
 
         {/* gradient image */}
         <div className="w-full h-full absolute top-0 left-0 z-[2] bg-black/70"></div>
 
-        <div className="min-h-screen flex flex-col justify-start items-center z-[10] relative">
+        <div className="min-h-screen flex flex-col justify-start items-center z-[100] relative">
           {/* hero section content */}
           <div className="w-[80%] lg:w-[60%] flex flex-col text-center gap-2">
             <div className="grid place-items-center">
@@ -62,85 +63,98 @@ export default function Home() {
             </div>
           </div>
 
-          {/* cat animation video section */}
-          <div className="">
-            <video
-              autoPlay
-              loop
-              muted
-              className="w-96 h-96 aspect-video object-contain"
-              src="/assets/animation/cat-animation.mp4"
-            >
-            </video>
-          </div>
+          <div className="custom-gradient w-full">
+            {/* cat animation video section */}
+            <div className="hidden lg:flex justify-center items-center mx-auto ">
+              <div className="rounded-full overflow-hidden">
 
-          {/* social icons section */}
-          <div className="flex items-center gap-14 text-white my-10">
-            <FaXTwitter className="text-3xl lg:text-6xl transition-all duration-200 hover:scale-105 hover:text-white/50" />
-            <div className="p-2 rounded-full bg-white text-black">
-              <RiSendPlaneFill className="text-2xl lg:text-5xl transition-all duration-200 hover:scale-105 hover:text-blue-400" />
+                <video
+                  autoPlay
+                  loop
+                  muted
+                  controls={false}
+                  playsInline
+                  className="w-96 h-96 aspect-video object-contain"
+                  src="/assets/animation/cat-animation.mp4"
+                >
+
+                </video>
+              </div>
+
             </div>
-            <FaGithub className="text-3xl lg:text-6xl transition-all duration-200 hover:scale-105 hover:text-white/50" />
-          </div>
 
-          {/* gradient text section */}
-          <div className="grid place-items-center px-20 lg:px-24 my-10 overflow-hidden">
-            <div
-              className="rounded-4xl bg-[#171D27] px-4 lg:px-14 py-4"
-              data-aos="fade-up"
-            >
-              <img
-                src="/assets/icon-images/first-qoute.png"
-                className="w-3 lg:w-auto"
-                alt=""
-              />
-              <h2 className="text-center lg:text-[45px] leading-tight font-bold bg-gradient-to-b from-white to-white/30 bg-clip-text text-transparent lg:px-10">
-                Claw brings the cyberpunk AI revolution. A fusion of AI
-                intelligence and degen crypto spirit.
-              </h2>
-              <div className="flex justify-end">
+            {/* social icons section */}
+            <div className="w-full flex justify-center items-center gap-14 text-white my-10">
+              <FaXTwitter className="text-3xl lg:text-6xl transition-all duration-200 hover:scale-105 hover:text-white/50" />
+              <div className="p-2 rounded-full bg-white text-black">
+                <RiSendPlaneFill className="text-2xl lg:text-5xl transition-all duration-200 hover:scale-105 hover:text-blue-400" />
+              </div>
+              <FaGithub className="text-3xl lg:text-6xl transition-all duration-200 hover:scale-105 hover:text-white/50" />
+            </div>
+
+            {/* gradient text section */}
+            <div className="grid place-items-center px-20 lg:px-24 my-10 overflow-hidden">
+              <div
+                className="rounded-4xl bg-[#171D27] px-4 lg:px-14 py-4"
+                data-aos="fade-up"
+              >
                 <img
-                  src="/assets/icon-images/second-qoute.png"
-                  className="w-3 lg:w-auto lg:mr-14"
+                  src="/assets/icon-images/first-qoute.png"
+                  className="w-3 lg:w-auto"
                   alt=""
                 />
+                <h2 className="text-center lg:text-[45px] leading-tight font-bold bg-gradient-to-b from-white to-white/30 bg-clip-text text-transparent lg:px-10">
+                  Claw brings the cyberpunk AI revolution. A fusion of AI
+                  intelligence and degen crypto spirit.
+                </h2>
+                <div className="flex justify-end">
+                  <img
+                    src="/assets/icon-images/second-qoute.png"
+                    className="w-3 lg:w-auto lg:mr-14"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* video section */}
+            <div className="w-full relative mt-5 min-h-screen py-10 flex flex-col justify-between">
+              {/* example Iframe */}
+              <CustomIframe />
+              {/* footer */}
+
+              <video
+                src="/assets/icon-images/Cyber City Video.mp4"
+                className="absolute top-0 left-0 w-full h-full opacity-30 z-0 object-cover"
+                autoPlay
+                loop
+                muted
+                controls={false}
+                playsInline
+              ></video>
+
+              <div className="flex items-center justify-center gap-5 z-[100]">
+                <a href="#hero-section" className="cursor-pointer">
+                  <img
+                    src="/assets/icon-images/$CLAW-1.png"
+                    className="w-40 lg:w-60 object-contain"
+                    alt=""
+                  />
+                </a>
+                <hr className="w-px h-10 bg-white -ml-10" />
+                <a href="#" target="_blank">
+                  <img
+                    src="/assets/icon-images/solana-sol-logo.png"
+                    className="w-14 lg:w-20 object-contain"
+                    alt=""
+                  />
+                </a>
               </div>
             </div>
           </div>
-
-          {/* video section */}
-          <div className="w-full relative mt-5 min-h-screen py-10 flex flex-col justify-between">
-            {/* example Iframe */}
-            <CustomIframe />
-            {/* footer */}
-
-            <video
-              src="/assets/icon-images/Cyber City Video.mp4"
-              className="absolute top-0 left-0 w-full h-full opacity-30 z-0 object-cover"
-              autoPlay
-              loop
-              muted
-            ></video>
-
-            <div className="flex items-center justify-center gap-5 z-[100]">
-              <a href="#hero-section" className="cursor-pointer">
-                <img
-                  src="/assets/icon-images/$CLAW-1.png"
-                  className="w-40 lg:w-60 object-contain"
-                  alt=""
-                />
-              </a>
-              <hr className="w-px h-10 bg-white -ml-10" />
-              <a href="#" target="_blank">
-                <img
-                  src="/assets/icon-images/solana-sol-logo.png"
-                  className="w-14 lg:w-20 object-contain"
-                  alt=""
-                />
-              </a>
-            </div>
-          </div>
         </div>
+        {/* pattern */}
+        <img src="/assets/icon-images/bg-pattern.png" className="absolute top-0 left-0 w-full h-full object-cover z-10 pointer-events-none" alt="" />
       </main>
 
       {/* footer */}
